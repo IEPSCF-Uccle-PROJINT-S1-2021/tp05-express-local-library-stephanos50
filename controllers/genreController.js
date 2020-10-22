163cb1fe0d9b862eefbb9605df6bec76bbbed520
stefan <stefan@iMac-de-stefan.lan>
2020-10-22 09:13:31 +0200

    genre_list

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -2,8 +2,16 @@ const { Book, Genre } = require("../models/sequelize");
 const createError = require("http-errors");
 
 // Display list of all Genre.
-exports.genre_list = function (req, res) {
-  res.send("NOT IMPLEMENTED: Genre list");
+exports.genre_list = async function (req, res, next) {
+    try {
+      const genre_list = await Genre.findAll();
+      res.render('genre_list', {
+        title: 'Genre List',
+        genre_list: genre_list,
+    });
+    } catch (error) {
+      next(error);
+    }
 };
 
 // Display detail page for a specific Genre.
